65b74b8ebe45a6c43c8bd7711d8baee1071a4afa
Pedreb <pedrohueb12@hotmail.com>
2025-03-10 13:13:14 -0300

    Add files via upload

--- almoxeletrico.py
+++ almoxeletrico.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import sqlite3
 import pandas as pd
-from xlsxwriter import Workbook
 
 # Função para criar conexão com o banco de dados e ativar FOREIGN KEY
 def get_db_connection():
